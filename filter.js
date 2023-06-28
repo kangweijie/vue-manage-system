5ea9d55d9e9ebb52acf4a5add46fac02833fa60d
kangweijie <839354343@qq.com>
2023-06-28 18:03:08 +0800

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,2 +1,6 @@
 document.body.style.backgroundColor = "red";
+var style = document.createElement('style'); 
+ style.type = 'text/css'; 
+ style.innerHTML="img{ display:none!important }"; 
+ document.getElementsByTagName('head').item(0).appendChild(style); 
 
