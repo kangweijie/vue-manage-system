0a2a83f201e63a01383610320b1821f84a00d2a7
kangweijie <839354343@qq.com>
2023-06-28 18:01:36 +0800

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,8 +1,2 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
-});
+document.body.style.backgroundColor = "red";
 
