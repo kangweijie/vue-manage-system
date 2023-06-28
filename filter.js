2b8552a6504afd5a0136132f99cbbcb78bca4d86
kangweijie <839354343@qq.com>
2023-06-28 17:12:28 +0800

    Update filter.js

--- filter.js
+++ filter.js
@@ -2,3 +2,5 @@ var style = document.createElement("style");
 style.type = "text/css";
 style.innerHTML = "img{ display:none!important }";
 document.getElementsByTagName("head").item(0).appendChild(style);
+console.log('ok')
+
