a91ab7a0878f75753c68ab50d90d0988d447d6b9
kangweijie <839354343@qq.com>
2023-06-28 17:24:46 +0800

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,6 +1,17 @@
 var style = document.createElement("style");
 style.type = "text/css";
 style.innerHTML = "img{ display:none!important }";
-document.getElementsByTagName("head").item(0).appendChild(style);
-alert('ok')
+
+var greeting = "您好啊，";
+var button = document.getElementById("mybutton");
+button.person_name = "Roberto";
+button.addEventListener(
+  "click",
+  function () {
+    document.getElementsByTagName("head").item(0).appendChild(style);
+    alert(greeting + button.person_name + "。");
+    console.log("ok");
+  },
+  false
+);
 
